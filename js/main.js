451912238cc3c5af2c2e409310c54ede6e73b668
Ashit-10 <95882932+Ashit-10@users.noreply.github.com>
2024-12-02 13:36:44 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@
         }, 1);
     };
     spinner();
-    
-    
+
+
     // Initiate the wowjs
     new WOW().init();
 
@@ -24,8 +24,8 @@
             $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
         }
     });
-    
-    
+
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
@@ -35,7 +35,7 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
 
@@ -54,8 +54,8 @@
         items: 1,
         dots: true,
         loop: true,
-        nav : true,
-        navText : [
+        nav: true,
+        navText: [
             '<i class="bi bi-chevron-left"></i>',
             '<i class="bi bi-chevron-right"></i>'
         ]
@@ -69,17 +69,17 @@
         center: true,
         dots: false,
         loop: true,
-        nav : true,
-        navText : [
+        nav: true,
+        navText: [
             '<i class="bi bi-arrow-left"></i>',
             '<i class="bi bi-arrow-right"></i>'
         ],
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             },
-            768:{
-                items:2
+            768: {
+                items: 2
             }
         }
     });
@@ -94,8 +94,32 @@
         $("#portfolio-flters li").removeClass('active');
         $(this).addClass('active');
 
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
+        portfolioIsotope.isotope({ filter: $(this).data('filter') });
     });
-    
+
 })(jQuery);
 
+document.getElementById('bookingForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const appliance = document.getElementById('appliance').value;
+    const phone = document.getElementById('phone').value;
+    const name = document.getElementById('name').value;
+    const address = document.getElementById('address').value;
+
+    const message = `#New_Booking:\n\nAppliance: ${appliance}\nName: ${name}\nPhone: ${phone}\nAddress: ${address}`;
+    console.log(message);
+    // Optional: Send data to Telegram (uncomment and adjust for your bot)
+
+    const telegramMessage = encodeURIComponent(message);
+    // const telegramBotToken = '7589123215:AAG6E-x1NqACiw2MBY3L2fsnWCsp6TQTTgY';
+    // const chatId = '-1002168382676';
+    const telegramBotToken = '2113644798:AAEuMDxeifbAtrzka5UsM4K4CHm4qqOBjUI';
+    const chatId = '-1001417526920';
+    fetch(`https://api.telegram.org/bot${telegramBotToken}/sendMessage?chat_id=${chatId}&text=${telegramMessage}`);
+
+    // Show the submit banner
+    // Show the alert
+    alert("Your booking will be processed and confirmed shortly!");
+    // Reload the page
+});
